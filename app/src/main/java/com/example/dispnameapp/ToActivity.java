1de74be507f746eda9ef5ed37da3394a4f0b29a9
atom19-i <54570007+atom19-i@users.noreply.github.com>
2020-10-15 15:31:40 +0530

    redirection to second activity is added

---
+++ app/src/main/java/com/example/dispnameapp/ToActivity.java
@@ -0,0 +1,18 @@
+package com.example.dispnameapp;
+
+import android.app.Activity;
+import android.view.View;
+
+import java.util.Random;
+
+public class ToActivity extends Activity {
+
+
+    //String[] names = {"Iti","Lionel","Aguero","Kevin","Ronaldo"} ;
+
+
+
+    //private View context;
+   // String[] array = context.getResources().getStringArray(R.array.names);
+    //String randomStr = array[new Random().nextInt(array.length)];
+ }

fe00461a523376bc4ab6725b90392a9ebec5c61c
atom19-i <54570007+atom19-i@users.noreply.github.com>
2020-10-15 13:50:05 +0530

    click button added on main activity

---
+++ app/src/main/java/com/example/dispnameapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dispnameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
